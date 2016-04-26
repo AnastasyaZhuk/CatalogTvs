176aed67ccb2afb3266bcf5ceb0990e1d91812f1
anaydenova <anaydyonova@gmail.com>
2016-04-26 16:44:59 +0300

    Added check password complexity

--- register/register.js
+++ register/register.js
@@ -14,25 +14,47 @@ angular.module('myApp.register', ['ngRoute', 'firebase'])
     .controller('RegisterCtrl', ['$scope', '$location', '$firebaseAuth', function ($scope, $location, $firebaseAuth) {
         var firebaseObj = new Firebase("https://radiant-torch-2188.firebaseIO.com");
         var auth = $firebaseAuth(firebaseObj);
+
         $scope.signUp = function () {
-            if (!$scope.regForm.$invalid) {
-                var email = $scope.user.email;
-                var password = $scope.user.password;
-                if (email && password) {
-                    auth.$createUser(email, password)
-                        .then(function () {
-                            alert("Пользователь успешно создан");
-                            $location.path('/home');
-                        }, function (error) {
-                            var emailInUse = "The specified email address is already in use.";
-                            $scope.regError = true;
-                            if (error.message == emailInUse) {
-                                $scope.regErrorMessage = "Указанный адрес электронной почты уже используется."
-                            }
-                            else {
-                                $scope.regErrorMessage = error.message;
-                            }
-                        });
+            var password = $scope.user.password;
+            var s = 0;
+            if (password.length >= 7) {
+                s++;
+            }
+            if (/[0-9]/.test(password))
+                s++;
+            if (/[a-z]/.test(password))
+                s++;
+            if (/[A-Z]/.test(password))
+                s++;
+            if (/[^A-Z-0-9]/i.test(password))
+                s++;
+            if (s < 5) {
+                $scope.error = true;
+                $scope.errorMessage = "Пароль должен содержать: не меньше 7 символов, заглавную и прописную букву, цифру, знак преписания!";
+            }
+
+
+            if (s >= 5) {
+                $scope.error = false;
+                if (!$scope.regForm.$invalid) {
+                    var email = $scope.user.email;
+                    if (email && password) {
+                        auth.$createUser(email, password)
+                            .then(function () {
+                                alert("Пользователь успешно создан");
+                                $location.path('/home');
+                            }, function (error) {
+                                var emailInUse = "The specified email address is already in use.";
+                                $scope.regError = true;
+                                if (error.message == emailInUse) {
+                                    $scope.regErrorMessage = "Указанный адрес электронной почты уже используется."
+                                }
+                                else {
+                                    $scope.regErrorMessage = error.message;
+                                }
+                            });
+                    }
                 }
             }
         };
