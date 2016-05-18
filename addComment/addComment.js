87446a5867ab807159e2347a94a2ced96aa6dda6
anaydenova <anaydyonova@gmail.com>
2016-05-18 12:32:35 +0300

    добавлены комментарии

--- addComment/addComment.js
+++ addComment/addComment.js
@@ -4,12 +4,15 @@ angular.module('myApp.addComment', ['ngRoute'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/:tvId/comments', {
+            // Если url заканчивается /:tvId/comments, то перейти на отображение addComment.html, использовать контроллер AddCommentCtrl
             templateUrl: 'addComment/addComment.html',
             controller: 'AddCommentCtrl'
         });
     }])
 
+    // контроллер AddCommentCtrl
     .controller('AddCommentCtrl', ['$scope', 'CommonProp', '$routeParams', '$firebase', function ($scope, CommonProp, $routeParams, $firebase) {
+       // Если кнопка "Новый отзыв" нажата, то отобразить форму
         $("button").on("click", function () {
             var state = $(this).data('state');
             state = !state;
@@ -20,24 +23,31 @@ angular.module('myApp.addComment', ['ngRoute'])
             }
             $(this).data('state', state);
         });
+        // получить id текущего товара
         $scope.idTv = $routeParams.tvId;
+        // ссылка для чтения и записи из Firebase
         var fb = $firebase(new Firebase("https://radiant-torch-2188.firebaseio.com/Comments"));
+        // Получаем список объектов из Firebase
         $scope.comments = fb.$asObject();
 
+        // Метод для добавления нового отзыва, срабатывает при нажатии на кнопку "Оставить отзыв"
         $scope.AddComment = function () {
+            // Получаем значение полей Заголовок и Комментарии
             var title = $scope.comment.title;
             var desc = $scope.comment.desc;
+            // Создаем новый объект
             fb.$push({
-                idTv: $scope.idTv,
-                title: title,
-                desc: desc,
-                user: CommonProp.getUser()
+                idTv: $scope.idTv, //  id объекта
+                title: title, // заголовок
+                desc: desc, // комментарии
+                user: CommonProp.getUser() // пользователь, под которым находимся в системе
             }).then(function (ref) {
+                // если нет ошибки, очистить значение полей и закрыть форму
                 $scope.comment.title = '';
                 $scope.comment.desc = '';
                 $("span").removeClass("show");
-                // alert("Отзыв успешно добавлен");
             }, function (error) {
+                // если произошла ошибка, то вывести
                 alert("Произошла ошибка " + error);
             });
         };
--- detail/detail.js
+++ detail/detail.js
@@ -2,44 +2,47 @@
 
 angular.module('myApp.detail', ['ngRoute','ngAnimate','ngTouch'])
 
-    
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/detail/:tvId', {
+        // Если адрес /detail/:tvId, то используем detail.html и контроллер TVDetailCtrl
         templateUrl: 'detail/detail.html',
         controller: 'TVDetailCtrl'
-    }).otherwise({
+    }).otherwise({ // иначе возвращаемся на страницу каталога
         redirectTo: '/catalog'
     });
 }]);
 
+// контроллер TVDetailCtrl
 tvControllers.controller('TVDetailCtrl', ['$scope', '$routeParams','$http',
     function ($scope, $routeParams, $http) {
         console.log("TVDetailCtrl");
+        // Получаем id товара, на который перешли
         $scope.idTv = $routeParams.tvId;
+        // Получаем данные из json файла по id товара
         $http.get('json/' + $routeParams.tvId + '.json').success(function (data) {
             $scope.tvs = data;
             $scope.photos = $scope.tvs.images;
       });
 
-        // initial image index
+        // инициализация индекса для фотографии
         $scope._Index = 0;
 
-        // if a current image is the same as requested image
+        // Если текущее изображение является таким же, как выбранное изображение
         $scope.isActive = function (index) {
             return $scope._Index === index;
         };
 
-        // show prev image
+        // показать предыдущее изображение
         $scope.showPrev = function () {
             $scope._Index = ($scope._Index > 0) ? --$scope._Index : $scope.photos.length - 1;
         };
 
-        // show next image
+        //  показать следующее изображение
         $scope.showNext = function () {
             $scope._Index = ($scope._Index < $scope.photos.length - 1) ? ++$scope._Index : 0;
         };
 
-        // show a certain image
+        //показать выбранное изображение
         $scope.showPhoto = function (index) {
             $scope._Index = index;
         };
